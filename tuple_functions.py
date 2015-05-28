8cd5935d7f6333c6192b002aceb65b861bc1f583
Justice Lenon <justicelenon@Kelles-MacBook-Air.local>
2015-05-28 14:45:39 -0400

    tuple_functions.py

--- tuple_functions.py
+++ tuple_functions.py
@@ -1,8 +1,7 @@
 '''
 Several useful functoins for doing things with tuples
 '''
-
-def tuple_value_change(tup, x):
+def tuple_value_change (tup, x) :
     newtup = (x,)
     print tup
     print x
@@ -10,3 +9,14 @@ def tuple_value_change(tup, x):
     newtup = newtup + tup
     return newtup
 
+def tuple_lengths(*tup_len):
+
+    listoflength =[]
+    
+    #Iterating over each item in tup_len
+    for i in tup_len:
+        print i
+        print len(i)
+        listoflength.append(len(i))
+    
+    return listoflength
