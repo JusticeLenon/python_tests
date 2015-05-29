ecdc6ef0f634f4ee947d01dd5d2b1e3bde68ccbb
Justice Lenon <justicelenon@Kelles-MacBook-Air.local>
2015-05-29 09:10:37 -0400

    testing_plot.py

---
+++ testing_plot.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib as mpl
+x_range = np.arange(0.1,10.,0.01)
+y_range = np.sin(x_range)
+from numpy import *
+from matplotlib import *
+import matplotlib.pyplot as plt
+plt.plot(x_range,y_range)
+plt.show()
